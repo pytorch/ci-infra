89b0e8d88c3da99e4d3ccec2e388c4a40d4f119a
Jean Schmidt <contato@jschmidt.me>
2024-01-25 20:47:38 +0100

    .

--- aws/391835788720/us-east-1/locals.tf
+++ aws/391835788720/us-east-1/locals.tf
@@ -1,12 +1,12 @@
 locals {
-    availability_zones_loc = ["1a", "1b", "1c", "1d", "1f"]
+    availability_zones_loc = ["a", "b", "c", "d", "f"]
     availability_zones        = [
         for loc in local.availability_zones_loc :
-        "${local.aws_region}-${loc}"
+        "${local.aws_region}${loc}"
     ]
-    availability_zones_canary_loc = ["1b", "1d", "1f"]
+    availability_zones_canary_loc = ["b", "d", "f"]
     availability_zones_canary = [
         for loc in local.availability_zones_canary_loc :
-        "${local.aws_region}-${loc}"
+        "${local.aws_region}${loc}"
     ]
 }
