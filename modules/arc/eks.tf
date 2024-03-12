f07983ad285bc3957331c315d9b5d7d8ddb57d1a
Thanh Ha <thanh.ha@linuxfoundation.org>
2024-03-12 16:57:32 -0400

    ci: Set "green" nodegroup min size to 2 (#87)
    It seems Karpenter has an anti-node affinity requiring that both its
    instances run on different nodes. This is not possible when minimum
    node is only 1. Bump to 2 to allow Karpenter to run properly.
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- modules/arc/eks.tf
+++ modules/arc/eks.tf
@@ -54,9 +54,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     green = {
-      min_size     = 1
+      min_size     = 2
       max_size     = 20
-      desired_size = 1
+      desired_size = 2
 
       taints = [
         {
