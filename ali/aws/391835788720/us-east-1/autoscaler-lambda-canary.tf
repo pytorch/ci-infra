f275574c4e42e02dad4914163d01c36280c7995c
Thanh Ha <thanh.ha@linuxfoundation.org>
2024-05-29 13:57:13 -0400

    feat: Create a canary instance of ALI (#201)
    This creates a canary instance of ALI for testing and hacking.
    
    Closes: #199
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- ali/aws/391835788720/us-east-1/autoscaler-lambda-canary.tf
+++ ali/aws/391835788720/us-east-1/autoscaler-lambda-canary.tf
@@ -1,42 +1,34 @@
-# TODO create this app and import the secret to the account
-data "aws_secretsmanager_secret_version" "app_creds" {
-  secret_id = "pytorch-gha-infra-app-secrets"
-}
 
-resource "random_password" "webhook_secret" {
-  length = 28
-}
-
-module "autoscaler-lambda" {
+module "autoscaler-lambda-canary" {
   source = "../../../tf-modules/terraform-aws-github-runner"
 
   aws_region           = local.aws_region
   aws_region_instances = var.ali_aws_region
   vpc_ids = [
-    for module_def in module.ali_runners_vpc :
+    for module_def in module.ali_runners_canary_vpc :
     { vpc = module_def.vpc_id, region = local.aws_region }
   ]
   vpc_sgs = []
   subnet_vpc_ids = flatten([
-    for module_def in module.ali_runners_vpc :
+    for module_def in module.ali_runners_canary_vpc :
     flatten([
       for public_subnet in module_def.public_subnets :
       { subnet = public_subnet, vpc = module_def.vpc_id }
     ])
   ])
   subnet_azs = flatten([
-    for module_def in module.ali_runners_vpc :
+    for module_def in module.ali_runners_canary_vpc :
     module_def.subnet_azs
   ])
   vpc_cidrs           = [
-    for module_def in module.ali_runners_vpc:
+    for module_def in module.ali_runners_canary_vpc:
       {vpc = module_def.vpc_id, cidr = module_def.vpc_cidr}
   ]
-  lambda_subnet_ids         = module.ali_runners_vpc[var.aws_vpc_suffixes[0]].private_subnets
+  lambda_subnet_ids         = module.ali_runners_canary_vpc[var.aws_vpc_suffixes[0]].private_subnets
 
-  environment = var.ali_prod_environment
+  environment = var.ali_canary_environment
   tags = {
-    Project = "pytorch/pytorch"
+    Project = "pytorch/pytorch-canary"
   }
 
   instance_type = "c5.4xlarge"
@@ -51,9 +43,9 @@ module "autoscaler-lambda" {
     webhook_secret = random_password.webhook_secret.result
   }
 
-  webhook_lambda_zip                = "../../../assets/lambdas-download/webhook.zip"
-  runner_binaries_syncer_lambda_zip = "../../../assets/lambdas-download/runner-binaries-syncer.zip"
-  runners_lambda_zip                = "../../../assets/lambdas-download/runners.zip"
+  webhook_lambda_zip                = "../../../assets/lambdas-download-canary/webhook.zip"
+  runner_binaries_syncer_lambda_zip = "../../../assets/lambdas-download-canary/runner-binaries-syncer.zip"
+  runners_lambda_zip                = "../../../assets/lambdas-download-canary/runners.zip"
   enable_organization_runners       = false
   minimum_running_time_in_minutes   = 10
   runner_extra_labels               = "pytorch.runners"
@@ -64,7 +56,7 @@ module "autoscaler-lambda" {
   runners_scale_up_sqs_message_ret_s      = 7200
   scale_down_schedule_expression          = "cron(*/15 * * * ? *)"
   cant_have_issues_labels                 = ["Use Canary Lambdas"]
-  scale_config_repo_path                  = ".github/lf-scale-config.yml"
+  scale_config_repo_path                  = ".github/lf-c-scale-config.yml"
 
   encrypt_secrets           = false
   secretsmanager_secrets_id = data.aws_secretsmanager_secret_version.app_creds.secret_id
