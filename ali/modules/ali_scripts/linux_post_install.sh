a8fd299679600e1113b1a0df6dd8ddf5b05e770c
Jean Schmidt <4520845+jeanschmidt@users.noreply.github.com>
2024-06-11 16:44:29 +0200

    Use amazon linux 2023 (#222)

--- ali/modules/ali_scripts/linux_post_install.sh
+++ ali/modules/ali_scripts/linux_post_install.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+set +e
+
 # Log in to our ECR instance
-$(aws ecr get-login --no-include-email --region us-east-1)
+if uname -a | grep 'amzn2023' > /dev/null ; then
+    echo "New amazon linux"
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 308535385114.dkr.ecr.us-east-1.amazonaws.com
+else
+    echo "Old amazon linux"
+    $(aws ecr get-login --no-include-email --region us-east-1)
+fi
+
