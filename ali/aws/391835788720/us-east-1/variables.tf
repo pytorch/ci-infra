c66213188b89326bda16cd4cf923d41f8187c04d
Jean Schmidt <4520845+jeanschmidt@users.noreply.github.com>
2024-06-25 18:27:36 +0200

    Rollback AMZ linux 2023 (#233)

--- ali/aws/391835788720/us-east-1/variables.tf
+++ ali/aws/391835788720/us-east-1/variables.tf
@@ -38,7 +38,7 @@ variable "aws_canary_vpc_suffixes" {
 variable "ami_filter_linux" {
   description = "AMI for linux"
   type        = list
-  default     = ["al2023-ami-2023.4.20240528.0-kernel-6.1-x86_64"]
+  default     = ["amzn2-ami-hvm-2.0.20240306.2-x86_64-ebs"]
 }
 
 variable "ami_filter_windows" {
