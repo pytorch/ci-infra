b0ce97983f3ff7a3e0e74960561754023874b967
Jessica Wagantall <jwagantall@linuxfoundation.org>
2024-11-06 11:31:40 -0800

    CI: Block public access to S3 bucket
    This configuration is to address CKV2_AWS_6
    Configure new resource to:
    - Block public ACLs
    - Block public bucket policies
    - Ignore public ACLs that may exist
    - Ensures that the bucket cannot be publicly accessible
    
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- modules/backend-state/buckets.tf
+++ modules/backend-state/buckets.tf
@@ -10,3 +10,13 @@ resource "aws_s3_bucket" "terraform_state" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "access_terraform_state" {
+  count =  data.external.terraform_state_bucket_exists.result.exists == "true" ? 0 : 1
+  bucket = local.terraform_state_bucket_name
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
