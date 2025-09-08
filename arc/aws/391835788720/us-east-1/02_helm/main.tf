3a2d9a625631497c2487c2ded8c5645cc187d5f4
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-09-08 17:01:38 +0100

    Configure the k8s provider in the helm layer
    The helm layer in the ARC infra uses the k8s service
    resources to fetch and verify the Argo service name.
    
    Configure the k8s provider so that that may work.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- arc/aws/391835788720/us-east-1/02_helm/main.tf
+++ arc/aws/391835788720/us-east-1/02_helm/main.tf
@@ -24,6 +24,23 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.runners[0].outputs.cluster_endpoint
+  cluster_ca_certificate  = base64decode(data.terraform_remote_state.runners[0].outputs.cluster_ca_certificate)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.terraform_remote_state.runners[0].outputs.cluster_name,
+    ]
+  }
+}
+
+
 provider "helm" {
   kubernetes {
     host                  = data.terraform_remote_state.runners[0].outputs.cluster_endpoint
