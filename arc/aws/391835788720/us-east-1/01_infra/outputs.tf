d8fea7e3205ef7612ca39d99ce17fe912185ec33
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-09-11 15:51:38 +0100

    Add the AWS LB controller (#367)
    Add the resource required provision the AWS LB controller. This
    controller requires a special policy/role to provision an AWS network
    load balancer on the fly when an Ingress of the right class is created.
    
    This PR adapts previous resources as well to work with the AWS LB and
    fixes an issue with the ClusterIssuer by moving its provisioning to helm
    layer to address a limitation of the kubernetes_manifest terraform
    module.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- arc/aws/391835788720/us-east-1/01_infra/outputs.tf
+++ arc/aws/391835788720/us-east-1/01_infra/outputs.tf
@@ -19,3 +19,18 @@ output "cluster_id" {
   description = "EKS cluster ID"
   value         = module.pytorch_arc_dev_eks.cluster_id
 }
+
+output "oidc_provider_arn" {
+  description = "ARN of the cluster OIDC provider"
+  value         = module.pytorch_arc_dev_eks.oidc_provider_arn
+}
+
+output "cluster_oidc_issuer_url" {
+  description = "URL of the OIDC Issuer"
+  value         = module.pytorch_arc_dev_eks.cluster_oidc_issuer_url
+}
+
+output "vpc_id" {
+  description = "The ID of the cluster vpc"
+  value       = module.arc_runners_vpc.vpc_id
+}
