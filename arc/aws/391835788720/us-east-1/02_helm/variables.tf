fae01fe8a0135532dff7ce886130f8d8cbe42c58
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-09-11 18:37:50 +0100

    Setup the GitHub connector for ArgoCD/Dex
    Use a GitHub OAuth to provide authentication for the ArgoCD
    dashboard for members of a GitHub team in the pytorch-fdn org.
    
    Configure the policy so that:
    - default is no access
    - members of the multicloud-wg team get readonly access
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- arc/aws/391835788720/us-east-1/02_helm/variables.tf
+++ arc/aws/391835788720/us-east-1/02_helm/variables.tf
@@ -21,3 +21,25 @@ variable "letsencrypt_issuer" {
   description = "Name of the cert-manager cluster issuer"
   default     = "letsencrypt-prod"
 }
+
+variable "argocd_dex_github_org" {
+    type        = string
+    description = "GitHub org used for ArgoCD Dex GitHub connector"
+    default     = "pytorch-fdn"
+}
+
+variable "argocd_dex_github_team" {
+    type        = string
+    description = "GitHub team with readonly access to ArgoCD"
+    default     = "multicloud-wg"
+}
+
+variable "argocd_dex_github_client_id" {
+    type        = string
+    description = "GitHub OAuth App Client Id ArgoCD Dex"
+}
+
+variable "argocd_dex_github_client_secret" {
+    type        = string
+    description = "GitHub OAuth App Client Id ArgoCD Dex"
+}
