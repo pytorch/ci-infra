5c221954b39fda506c9b0de5340fedb3c5fa4660
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-09-15 14:10:00 +0100

    Add ARC control plane via terraform (#370)
    Installs the ARC control plane.
    This does not yet create any RunnerScaleSet, it only prepares the
    terraform code that loads the required secrets from AWS Secret Manager,
    so that they can be later injected in the relevant namespaces.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

---
+++ arc/aws/391835788720/us-east-1/02_helm/arc.tf
@@ -0,0 +1,24 @@
+/*
+ * Installs ARC control plane. This reconciles RunnerScaleSets once they are defined
+ * Since GitHub credentials are part of the RunnerScaleSet CRD, this controller does
+ * not connect to GitHub until any RunnerScaleSet exists.
+ *
+ * ARC provides and helm chart for the RunnerScaleSets
+ */
+resource "helm_release" "arc" {
+  name       = "arc"
+  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
+  chart      = "gha-runner-scale-set-controller"
+  namespace  = "arc-system"
+  version    = "0.12.1"
+
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values/gha-runner-scale-set-controller.yaml")
+  ]
+
+  depends_on = [
+    null_resource.k8s_infra_ready,
+  ]
+}
