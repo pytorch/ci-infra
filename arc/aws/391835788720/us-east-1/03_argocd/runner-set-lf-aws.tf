6eb3336bd73e71a2da829f1ea8a16ce7486790f3
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-09-17 09:32:14 +0100

    Remove leading space from provider_path
    The provider_path for ARC runner scale sets included an
    unwanted leading space, causing path match to fail.
    
    In the arc-deploy-prod workflow, pass TF_VAR_git_revision
    to the make apply, so that the git_version var in terraform
    is consistent with the branch that the workflow is being
    executed from. This allows for testing the ApplicationSet
    git directory matrix from the branch as well.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- arc/aws/391835788720/us-east-1/03_argocd/runner-set-lf-aws.tf
+++ arc/aws/391835788720/us-east-1/03_argocd/runner-set-lf-aws.tf
@@ -6,8 +6,8 @@ module "argocd-runner-scale-set" {
   server_addr   = local.argocd_ready ? local.argocd_endpoint : null
   token         = local.argocd_ready ? local.argocd_terraform_sa_token : null
   organization  = "lf"
-  cluster       = "local"
+  cluster       = "in-cluster"
   namespace     = "lf-aws"
-  provider_path = " argocd/aws/391835788720/us-east-1"
+  provider_path = "argocd/aws/391835788720/us-east-1"
   git_revision  = var.git_revision
 }
