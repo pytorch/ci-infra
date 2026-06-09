8d3917e4c6f844b5d3158d6884dcb28982c33798
George Hong <georgehong@meta.com>
2026-06-08 18:18:25 -0700

    Update
    [ghstack-poisoned]

--- osdc/modules/numa-scheduler/deploy.sh
+++ osdc/modules/numa-scheduler/deploy.sh
@@ -40,13 +40,11 @@ CHART_TGZ="scheduler-plugins-${CHART_VERSION}.tgz"
 CHART_DIR=$(mktemp -d)
 trap 'rm -rf "$CHART_DIR"' EXIT
 
-if [[ ! -f "${CHART_DIR}/${CHART_TGZ}" ]]; then
-  echo "Downloading scheduler-plugins chart v${CHART_VERSION}..."
-  gh release download "v${CHART_VERSION}" \
-    --repo kubernetes-sigs/scheduler-plugins \
-    --pattern "${CHART_TGZ}" \
-    --dir "$CHART_DIR"
-fi
+echo "Downloading scheduler-plugins chart v${CHART_VERSION}..."
+gh release download "v${CHART_VERSION}" \
+  --repo kubernetes-sigs/scheduler-plugins \
+  --pattern "${CHART_TGZ}" \
+  --dir "$CHART_DIR"
 
 echo "Installing numa-scheduler (scheduler-plugins v${CHART_VERSION})..."
 helm_upgrade_if_changed numa-scheduler numa-scheduler \
